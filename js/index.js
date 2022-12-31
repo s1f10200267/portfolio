cf5e565353d252020692c6d481fd92f8765e709a
Kensuke Omori <s1F102002671@iniad.org>
2023-01-01 02:26:28 +0900

    tentative commit

--- js/index.js
+++ js/index.js
@@ -2,3 +2,20 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('.typed', {
+      strings: [
+        "Kensuke OMORI's"
+      ],
+      typeSpeed: 80,
+      backSpeed: 20,
+      startDelay: 200,
+    });
+});
+
+$('.fadeInTrigger').on('inview', function(event, isInView) {
+    if (isInView) {//表示領域に入った時
+      $(this).addClass('animate__animated animate__fadeIn');//クラス名が付与
+    }
+});
